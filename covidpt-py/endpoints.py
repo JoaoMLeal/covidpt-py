9e7740811f46582cb7ff5509db9f26680f65a85b
fc49445 <fc49445@alunos.fc.ul.pt>
2021-05-31 10:34:39 +0100

    Added api endpoints

---
+++ covidpt-py/endpoints.py
@@ -0,0 +1,13 @@
+REQUEST_PATHS = {
+    "county_list":                  "https://covid19-api.vost.pt/Requests/get_county_list/",
+    "entry_from_until":             "https://covid19-api.vost.pt/Requests/get_entry/{date_1}_until_{date_2}",
+    "entry":                        "https://covid19-api.vost.pt​/Requests​/get_entry​/{date}",
+    "entry_counties":               "https://covid19-api.vost.pt​/Requests​/get_entry_counties​/{date_1}_until_{date_2}",
+    "entry_county":                 "​https://covid19-api.vost.pt/Requests​/get_entry_county​/{date_1}_until_{date_2}_{county}",
+    "full_dataset":                 "​https://covid19-api.vost.pt/Requests​/get_full_dataset",
+    "full_dataset_counties":        "https://covid19-api.vost.pt​/Requests​/get_full_dataset_counties",
+    "last_update":                  "https://covid19-api.vost.pt​/Requests​/get_last_update",
+    "last_update_counties":         "https://covid19-api.vost.pt​/Requests​/get_last_update_counties",
+    "last_update_specific_county":  "https://covid19-api.vost.pt​/Requests​/get_last_update_specific_county​/{county}",
+    "status":                       "​https://covid19-api.vost.pt/Requests​/get_status",
+}
